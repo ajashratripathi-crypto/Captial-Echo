57730b3ebc2b0462eb80da5e6dd55f46791d055b
ajashratripathi-crypto <ajashra.tripathi@gmail.com>
2026-08-13 17:39:53 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 const nextConfig = {
   output: "export",
+
   images: {
     unoptimized: true,
   },
+
   basePath: "/Capital-Echo",
-  assetPrefix: "/Capital-Echo/",
+  assetPrefix: "/Capital-Echo",
+
+  trailingSlash: true,
 };
 
 export default nextConfig;
